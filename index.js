e6adc46e6ed445aec26f674c8c258263b1f7d454
JoseAugustoDev <75041668+JoseAugustoDev@users.noreply.github.com>
2024-05-01 13:36:48 -0300

    Modal
    Adicionei o Modal de entrada e Saida

--- index.js
+++ index.js
@@ -7,13 +7,25 @@ const entrada = document.querySelector(".entrada-input");
 const adicionar_saida = document.querySelector(".input-saida");
 const adicionar_entrada = document.querySelector(".input-entrada");
 
+const modal = document.getElementById("modal");
+const openModalButton = document.getElementById("open-modal");
+const closeModalButton = document.getElementById("close-modal");
+
+openModalButton.addEventListener("click", () => {
+    modal.style = "display: flex;"
+});
+
+closeModalButton.addEventListener("click", () => {
+    modal.style = "display: none;"
+});
+
 saudacao.innerHTML = "Olá, " + nome_usuario + "!";
 
 var aux = 0;
 var saldo_atualizado = 0;
 
 adicionar_entrada.addEventListener('click', () => {
-    var valor_entrada = parseFloat(prompt("Digite o valor da entrada: "));
+    const valor_entrada = parseFloat(document.querySelector('#valor-entrada').value);
 
     saldo_atualizado += valor_entrada;
     
@@ -21,9 +33,20 @@ adicionar_entrada.addEventListener('click', () => {
 
 })
 
+const modal_saida = document.querySelector(".modal-saida");
+const openModalButton_saida = document.querySelector(".open-modal-saida");
+const closeModalButton_saida = document.querySelector(".close-modal-saida");
+
+openModalButton_saida.addEventListener("click", () => {
+    modal_saida.style = "display: flex;"
+});
+
+closeModalButton_saida.addEventListener("click", () => {
+    modal_saida.style = "display: none;"
+});
 
 adicionar_saida.addEventListener('click', () => {
-    var valor_saida = parseFloat(prompt("Digite o valor da saida: "));
+    var valor_saida = parseFloat(document.querySelector('#valor-saida').value);
 
     saldo_atualizado -= valor_saida;
 
